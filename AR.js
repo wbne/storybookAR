b6d5df47cfcfb346f85206ddf04f497126b580ff
wbne <firetruckbw@gmail.com>
2021-06-12 16:15:35 -0500

    now tracks hiro-marker and makes funny building appear

--- AR.js
+++ AR.js
@@ -1,8 +1,9 @@
 const URL = "https://console.echoar.xyz/query?key=little-shape-9651"
 const IMAGE_ID =  "a1252437-d5ad-48c2-aca3-72dd697d2621"
-const FILE_LINK = "https://console.echoar.xyz/query?key=little-shape-9651&file=a1252437-d5ad-48c2-aca3-72dd697d2621"
+const FILE_LINK = "https://console.echoar.xyz/query?key=little-shape-9651&file="
 var echoDB
 var model
+var storageID
 
 window.addEventListener('load',
   function() {
@@ -11,8 +12,8 @@ window.addEventListener('load',
       .then((json) => {
         echoDB = json
         model = echoDB.db[IMAGE_ID]
-        //console.log(model)
+        storageID = ('glbHologramStorageFilename' in model['additionalData']) ? model["additionalData"]["glbHologramStorageID"] : model["hologram"]["storageID"]
         const entityHTML = document.querySelector("a-entity")
-        entityHTML.setAttribute("gltf-model", FILE_LINK)
+        entityHTML.setAttribute("gltf-model", FILE_LINK + storageID)
       })
   }, false);
