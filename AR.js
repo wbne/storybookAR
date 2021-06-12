fe319b24e80706b1e2f9aa2b65e9a1c2e7d3faf7
wbne <firetruckbw@gmail.com>
2021-06-12 16:58:52 -0500

    time to test image tracking :angry:
    also commented out API calls so I don't hit limit testing other stuff

--- AR.js
+++ AR.js
@@ -1,13 +1,13 @@
-const URL = "https://console.echoar.xyz/query?key=little-shape-9651"
+const API = "https://console.echoar.xyz/query?key=little-shape-9651"
 const IMAGE_ID =  "a1252437-d5ad-48c2-aca3-72dd697d2621"
 const FILE_LINK = "https://console.echoar.xyz/query?key=little-shape-9651&file="
 var echoDB
 var model
 var storageID
 
-window.addEventListener('load',
+/*window.addEventListener('load',
   function() {
-    fetch(URL)
+    fetch(API)
       .then((response) => response.json())
       .then((json) => {
         echoDB = json
@@ -16,4 +16,4 @@ window.addEventListener('load',
         const entityHTML = document.querySelector("a-entity")
         entityHTML.setAttribute("gltf-model", FILE_LINK + storageID)
       })
-  }, false);
+  }, false);*/
